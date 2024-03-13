39824b97b6dacd96badd9248ce0d66ae3a76d331
Alex Sousa <lexusmem@gmail.com>
2024-03-13 00:04:42 -0300

    Nova aula de POO - TDD e testes unitarios, tratamentos de erros e lançamentos de exceções

--- Analise e Desenvolvimento de Sistema/2_Semestre/Programação_Orientada_a_Objetos/erros_calcula_idade_11.py
+++ Analise e Desenvolvimento de Sistema/2_Semestre/Programação_Orientada_a_Objetos/erros_calcula_idade_11.py
@@ -1,24 +1,37 @@
 def incrementa_int(n):
     print('Função incrementa_int: n =', n)
     if not isinstance(n, int):
-        raise TypeError('O número deve ser inteiro.')
+        raise TypeError
     return n + 1
 
 
 def calcula_idade(idade):
-    nova_idade = incrementa_int(idade)
-    print('Esse código não é executado se der erro na função incrementa_int.')
-    return nova_idade
+    try:
+        nova_idade = incrementa_int(idade)
+    except TypeError:
+        print('O número deve ser inteiro.')
+        raise
+    except Exception:
+        print('Deu Erro!')
+        raise
+    else:
+        print(
+            'A função calcula_idade não é executado se der erro na função incrementa_int.')
+        return nova_idade
 
 
 def main():
     print('Executando a função principal...')
-    resposta = calcula_idade(20)
-    print('Esse código não será executado se der erro na função incrementa_int.')
-    print('A nova idade é:', resposta)
+    try:
+        resposta = calcula_idade(20.5)
+    except Exception:
+        print('Tente novamente....')
+    else:
+        print('A função main não será executado se der erro na função incrementa_int.')
+        print('A nova idade é:', resposta)
 
 
 if __name__ == '__main__':
     print('Chamando a função main()')
     main()
-    print('Não será executado se existir erro na função incrementa_int.')
+    print('Não deu erro no nosso programa, os erros ocorridos foram tratados.')
