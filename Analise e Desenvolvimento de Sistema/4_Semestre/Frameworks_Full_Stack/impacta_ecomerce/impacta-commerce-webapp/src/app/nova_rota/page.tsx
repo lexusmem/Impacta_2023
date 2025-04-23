a9f343b73d85adf3374f2922fbaf942b31205c3b
Alex Sousa <lexusmem@gmail.com>
2025-04-23 20:52:01 -0300

    Novas aulas de full stack

---
+++ Analise e Desenvolvimento de Sistema/4_Semestre/Frameworks_Full_Stack/impacta_ecomerce/impacta-commerce-webapp/src/app/nova_rota/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React, { useState } from 'react';
+
+export default function Nova_Rota() {
+
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount(count + 1);
+    }
+
+    return (
+
+        <div className='mx-4'>
+            <p>O valor do contador é: {count}</p>
+            <button
+                className='bg-blue-500 text-white p-2'
+                onClick={increment}>
+
+                Incrementar
+            </button>
+
+            <div>
+                <p>
+                    <a href="https://www.youtube.com/watch?v=e6FigV2fLC8&t=77s">Video Next JS</a>
+                </p>
+            </div>
+
+        </div>
+    );
+}
