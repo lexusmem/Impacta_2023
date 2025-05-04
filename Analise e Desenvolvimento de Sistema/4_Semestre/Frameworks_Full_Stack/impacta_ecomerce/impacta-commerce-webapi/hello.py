d704946e8db664c97bd597222e699a981ced3558
Alex Sousa <lexusmem@gmail.com>
2025-05-04 13:20:24 -0300

    Novas aulas de full stack

--- Analise e Desenvolvimento de Sistema/4_Semestre/Frameworks_Full_Stack/impacta_ecomerce/impacta-commerce-webapi/hello.py
+++ Analise e Desenvolvimento de Sistema/4_Semestre/Frameworks_Full_Stack/impacta_ecomerce/impacta-commerce-webapi/hello.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    produtos_url = '/produtos'
+    return f'<p><h2>Hello, World!</h2><p><a href="{produtos_url}">Produtos</a></p></p>'
+
+
+@app.route('/produtos')
+def produtos():
+    response = jsonify([
+        {
+            "title": "Caneca Personalizada de Porcelana  do back end",
+            "amount": 123.45,
+            "installments": {"number": 3, "total": 41.15, "hasFee": True},
+        },
+        {
+            "title": "Caneca de Tulipa do back end",
+            "amount": 123.45,
+            "installments": {"number": 3, "total": 41.15},
+        }
+    ])
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 
 if __name__ == '__main__':
